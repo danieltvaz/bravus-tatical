ed620e09d98435f5dabee618e7e35a0e34c3518b
danieltvaz <danieltostes@live.com>
2024-08-14 13:15:50 -0300

    fix: adjust image styles

--- src/components/page-container/index.tsx
+++ src/components/page-container/index.tsx
@@ -2,8 +2,9 @@ import { ReactNode } from "react";
 import styled from "styled-components";
 
 const Container = styled.main`
-  display: "flex";
-  flex-direction: "column";
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 `;
 
 export default function PageContainer({ children }: { children: ReactNode }) {
--- src/components/section-container/styles.ts
+++ src/components/section-container/styles.ts
@@ -2,5 +2,5 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   min-width: 100vw;
-  max-width: 100vw;
+  height: 100%;
 `;
